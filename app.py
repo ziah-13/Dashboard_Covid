a68f99e8314384a6a61c8cdf3a68f6a6edc2aed8
ziah-13 <zianafirst@gmail.com>
2026-04-24 14:35:37 +0700

    Update Fitur Baru

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from data import show_data, total_kasus, kolom
+from data import *
 
 def judul():
     st.title("📊 Dashboard Covid-19")
@@ -12,12 +12,20 @@ menu = st.sidebar.radio("Pilih Halaman", ["Home", "Halaman Data"])
 # halaman home
 if menu == "Home":
     judul()
+    year = select_year()
+    df = load_data()
+    df_filtered = filter_data(df, year)
+    kolom(df_filtered)
+    pie_chart1(df_filtered)
+
 
 # halaman data
 elif menu == "Halaman Data":
-    total_kasus()   
-    show_data()     
-    kolom()         
+    judul()
+    year = select_year()
+    df = load_data()
+    df_filtered = filter_data(df, year)
+    show_data(df_filtered)       
 
 # footer
 st.markdown("---")
@@ -25,3 +33,4 @@ st.markdown(
     "<center>© 2026 Mifthahul Fauziah - NPM 184240008</center>",
     unsafe_allow_html=True
 )
+
--- data.py
+++ data.py
@@ -1,39 +1,99 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as px
+
 
 def load_data():
     df = pd.read_csv("dataset/covid_19_indonesia_time_series_all.csv")
     return df
 
-def show_data():
-    df = load_data()
+def filter_data(df, year=None):
+    if year is not None:
+        df = df[df['Date'].astype(str).str.contains(str(year))]
+    return df
+
+def select_year():
+    return st.sidebar.selectbox(
+        "Pilih Tahun📅",
+        options=[None, 2020, 2021, 2022],
+        format_func=lambda x: "Semua Tahun" if x is None else str(x)
+    )
+
+def show_data(df):
+    selected_columns = ['Location'] + list(df.loc[:, 'New Cases':'Total Recovered'].columns)
+    df_selected = df[selected_columns]
     st.subheader("🦠 Data Kasus Covid-19 di Indonesia")
-    st.dataframe(df.head(10))
+    st.dataframe(df_selected.head(10))
     
     st.subheader("📈 Statistika Deskriptif Pada Dataset")
     st.write(df.describe())
 
-def total_kasus():
-    df = load_data()
-    total_kasus = df["Total Cases"].sum()
+#total kasus
+def total_case(df):
+    total_kasus = df["New Cases"].sum()
     st.subheader("📊 Total Kasus Keseluruhan")
-    st.success(f"Total seluruh kasus : {total_kasus:,}")
+    return total_kasus
 
-def kolom():
-    df = load_data()
-    kolom_tampil = [
-        "Location",
-        "New Cases",
-        "New Deaths",
-        "New Recovered",
-        "New Active Cases",
-        "Total Cases",
-        "Total Deaths",
-        "Total Recovered"
-    ]
-    df_filtered = df[kolom_tampil]
-    st.subheader("📋 Data Kolom Tertentu")
-    st.dataframe(df_filtered)
+#total kematian
+def total_death(df):
+    total_kematian = df['New Deaths'].sum()
+    return total_kematian
+
+#total sembuh
+def total_recovery(df):
+    total_sembuh = df['New Recovered'].sum()
+    return total_sembuh
+
+#kolom 1
+def kolom(df):
+    kasus = total_case(df)
+    kematian = total_death(df)
+    sembuh = total_recovery(df)
+    
+    co11, co12, co13 = st.columns(3)
+    co11.metric(label="Total Kasus 📈", value=kasus, border=True)
+    co12.metric(label="Total Kematian 💀", value=kematian, border=True)
+    co13.metric(label="Total Sembuh 😻", value=sembuh, border=True)
+    
+def pie_chart1(df):
+    total_kematian=total_death(df)
+    total_sembuh=total_recovery(df)
+    
+    data={
+        'Status' : ['Meninggal', 'Sembuh'],
+        'Jumlah' : [total_kematian, total_sembuh]
+    }
+    
+    fig = px.pie(
+        data,
+        names='Status',
+        values='Jumlah',
+        title='Perbandingan Total Kematian VS Total Kesembuhan',
+        hole=0.5,
+        color_discrete_sequence=['#4de89f', '#ff6459']
+    )
+    
+    st.plotly_chart(fig, use_container_width=True)
+
+# def kolom(df=None):
+#     # jika tidak dikirim df dari app.py, pakai load_data()
+#     if df is None:
+#         df = load_data()
+
+#     kolom_tampil = [
+#         "Location",
+#         "New Cases",
+#         "New Deaths",
+#         "New Recovered",
+#         "New Active Cases",
+#         "Total Cases",
+#         "Total Deaths",
+#         "Total Recovered"
+#     ]
+#     df_filtered = df[kolom_tampil]
+#     st.subheader("📋 Data Kolom Tertentu")
+#     st.dataframe(df_filtered)
+
 
 # footer 
 st.markdown("---")
